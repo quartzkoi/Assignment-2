13fa2b81e7a3b978ca1c0e51a9e884d8f3390015
quartzkoi <quartzkoi@gmail.com>
2023-02-02 10:41:32 -0600

    Updated, but not compleated

--- Assignment3.py
+++ Assignment3.py
@@ -2,7 +2,31 @@ import Shapes3D
 import csv
 
 class Solver:
-    with open("data.csv", mode ='r')as file:
+    total = 0
+    shapes = []
+
+    with open("example.csv", mode='r') as file:
         csvFile = csv.reader(file, delimiter=',')
         for lines in csvFile:
-            print(lines)
+            if lines[0] == "cube":
+                shapes.append(Shapes3D.Cube(float(lines[1])))
+            elif lines[0] == "cuboid":
+                shapes.append(Shapes3D.Cuboid(float(lines[1]), float(lines[2]), float(lines[3])))
+            elif lines[0] == "sphere":
+                shapes.append(Shapes3D.Sphere(float(lines[1])))
+            elif lines[0] == "cylinder":
+                shapes.append(Shapes3D.Cylinder(float(lines[1]), float(lines[2])))
+            elif lines[0] == "prism":
+                shapes.append(Shapes3D.Prism(float(lines[1]), float(lines[2]), float(lines[3])))
+            elif lines[0] == "area":
+                for shape in shapes:
+                    total += shape.area()
+                total *= float(lines[1])
+                shapes.clear()
+            elif lines[0] == "volume":
+                for shape in shapes:
+                    total += shape.volume()
+                total *= float(lines[1])
+                shapes.clear()
+
+    print(total)
--- Shapes3D.py
+++ Shapes3D.py
@@ -1,53 +1,51 @@
 import Shapes
 import math
 #Cuboid
-    #Cube, child of Cuboid
-#Cylinder / Sphere, both need circle
-#N-gonal prism
 class Cuboid:
     def __init__(self, height, width, length):
          self.width = width
          self.height = height    
          self.length = length
-    def GetArea(self):
-        return 2 *((self.width*self.length)(self.height*self.length)(self.height*self.width))
-    def GetVolume(self):
+    def area(self):
+        return 2 *((self.width * self.length)(self.height * self.length)(self.height * self.width))
+    def volume(self):
         return self.height * self.length * self.width
 
-class Clyinder:
-    def __init__(self, radius, height):
-        self.radius = radius
-        self.height = height
-        self.GetArea = self.GetArea()
-        self.GetVolume = self.GetVolume()
-    def GetArea(self):
-        return (2*math.pi*self.radius*self.height)+(2*math.pi*(self.radius**2))
-    def GetVolume(self):
-        return math.pi*(self.radius**2)*self.height
+#Cube, child of Cuboid
+class Cube(Cuboid):
+    def __init__(self, length):
+        super().__init__(length, length, length)
 
-class Sphere:
-    def __(self, radius):
-        self.radius = radius
-    def GetArea(self):
-        return (4*math.pi*(self.radius**2))
-    def GetVolume(self):
-        return ((4/3)*math.pi*(self.radius**3))
-
+#N-gonal prism
 class Prism:
     def __init__(self, height, sideLength, sides):
         self.height = height
         self.base = Shapes.Polygon(sideLength, sides).GetArea
         self.perimeter = Shapes.Polygon(sideLength,sides).GetPerimeter
 
-    def GetArea(self):
+    def area(self):
         return (self.perimeter * self.height) + (2 * self.base)
-    def GetVolume(self):
+    def volume(self):
         return self.base * self.height
 
-class Cube(Cuboid):
-    def __init__(self, length):
-        self.length = length
-    def GetArea(self):
-        return super().GetArea()
-    def GetVolume(self):
-        return super().GetVolume()
+#Cylinder
+class Clyinder:
+    def __init__(self, radius, height):
+        self.radius = radius
+        self.height = height
+        self.base = Shapes.Circle(radius).GetArea()
+        self.area = self.area()
+        self.volume = self.volume()
+    def area(self):
+        return (self.base * self.height)+(2 * self.base)
+    def volume(self):
+        return self.base*self.height
+
+#Sphere
+class Sphere:
+    def __(self, radius):
+        self.radius = radius
+    def area(self):
+        return (4 * math.pi * (self.radius**2))
+    def volume(self):
+        return ((4/3) * math.pi * (self.radius**3))
